c5b2d613f01f6f52efd9dcd3f1e9bc74f4fe87be
MIDHUN K <midhunixd@gmail.com>
2024-10-07 09:03:43 +0530

    Create multiply 2 numbers.java

---
+++ multiply 2 numbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class HelloWorld {
+    public static void main(String[] args) {
+      Scanner inp = new Scanner(System.in);
+      int a=inp.nextInt();
+      int b=inp.nextInt();
+      System.out.print(a*b);
+    }
+}
